f2bc84fa1a728e128d116917055a42cdcc3d434b
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2024-02-23 11:11:29 +0100

    practicing conditional rendering

--- react-props_greeting/src/App.js
+++ react-props_greeting/src/App.js
@@ -1,9 +1,29 @@
 import "./styles.css";
 
-export default function App() {
-  return <Greeting name="Nadja" />;
+// export default function App() {
+//   return (
+//     <div>
+//       <Greeting name="Mareike" />
+//     </div>
+//   );
+// }
+
+// function Greeting({ name }) {
+//   return (
+//     <div>
+//       <h1>Hello {name === "Mareike" ? "Coach" : { name }}!</h1>
+//     </div>
+//   );
+// }
+
+//conditional rendering
+
+import React from "react";
+
+function TemperatureDisplay({ temperature }) {
+  temperature = 27;
+  // Bedingtes Rendern für "Es ist heiß!" (Bonus)
+  return <div>{temperature > 25 ? "Es ist warm!" : "Es ist kühl!"}</div>;
 }
 
-function Greeting({ name }) {
-  return <h1>Hello {name} </h1>;
-}
+export default TemperatureDisplay;
