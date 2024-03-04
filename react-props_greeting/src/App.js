3d487ceee6f19a09d54b78e3eb807f68a3d54209
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2024-03-04 11:29:37 +0100

    conditional rendering with temperature

--- react-props_greeting/src/App.js
+++ react-props_greeting/src/App.js
@@ -1,29 +1,19 @@
 import "./styles.css";
 
-// export default function App() {
-//   return (
-//     <div>
-//       <Greeting name="Mareike" />
-//     </div>
-//   );
-// }
-
-// function Greeting({ name }) {
-//   return (
-//     <div>
-//       <h1>Hello {name === "Mareike" ? "Coach" : { name }}!</h1>
-//     </div>
-//   );
-// }
-
-//conditional rendering
-
-import React from "react";
-
-function TemperatureDisplay({ temperature }) {
-  temperature = 27;
-  // Bedingtes Rendern für "Es ist heiß!" (Bonus)
-  return <div>{temperature > 25 ? "Es ist warm!" : "Es ist kühl!"}</div>;
+export default function App() {
+  return (
+    <div>
+      <TemperatureDisplay temperature={31} />
+    </div>
+  );
 }
 
-export default TemperatureDisplay;
+function TemperatureDisplay({ temperature }) {
+  return (
+    <div>
+      <h1>
+        It's {temperature > 25 ? "warm!" : temperature > 30 ? "hot" : "cold!"}
+      </h1>
+    </div>
+  );
+}
--- react-props_greeting/src/index.js
+++ react-props_greeting/src/index.js
@@ -2,13 +2,12 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import TemperatureDisplay from "./App";
+
 const rootElement = document.querySelector("#root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
     <App />
-    <TemperatureDisplay />
   </StrictMode>
 );
